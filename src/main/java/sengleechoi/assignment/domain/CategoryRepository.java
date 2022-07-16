5984cd15d2fd9c496ac88020544e9ad189f9ca68
Senglee Choi <tmdfl1027@gmail.com>
2022-07-17 01:26:57 +0900

    Feat: fetch join으로 전체 카테고리를 가져오도록 리포지토리 수정
    이후 서비스 계층에서 하위 카테고리를 매핑하는 방식으로 개선 예정

--- src/main/java/sengleechoi/assignment/domain/CategoryRepository.java
+++ src/main/java/sengleechoi/assignment/domain/CategoryRepository.java
@@ -2,13 +2,12 @@ package sengleechoi.assignment.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query("select c1 from Category c1 left join fetch c1.parentCategory where c1.name = :name")
-    public List<Category> findCategoriesByName(@Param("name") String name);
+    @Query("select c1 from Category c1 left join fetch c1.parentCategory")
+    public List<Category> findAllCategories();
 }
